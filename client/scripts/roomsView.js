41c73d0153c13f494d20336b76cd1f0c6517322e
jpcastberg <jpcastberg@gmail.com>
2019-01-14 21:12:29 -0800

    Add rooms to Rooms.data

--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -2,8 +2,15 @@ var RoomsView = {
 
   $button: $('#rooms button'),
   $select: $('#rooms select'),
+  $input: $('#rooms input'),
 
   initialize: function() {
+    RoomsView.$button.on('click', RoomsView.addRoom.bind(RoomsView));
+  },
+  
+  addRoom: function() {
+    Rooms.data.push(this.$input.val());
+    this.$input.val('');
   },
 
   render: function() {
